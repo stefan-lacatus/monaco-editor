ec2a46c1c4822b044d1d6ee50fe3074356b0fc30
Petrisor Lacatus <placatus@iqnox.com>
2020-09-27 00:50:55 +0300

    fix metadata typo

--- metadata.js
+++ metadata.js
@@ -20,7 +20,7 @@
 					src: '/monaco-typescript/release/dev',
 					'npm/dev': 'node_modules/@placatus/monaco-typescript/release/dev',
 					'npm/min': 'node_modules/@placatus/monaco-typescript/release/min',
-					esm: 'node_modules/monaco-typescript/@placatus/release/esm',
+					esm: 'node_modules/@placatus/monaco-typescript/release/esm',
 				}
 			},
 			{
